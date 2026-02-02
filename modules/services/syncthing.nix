6d71aab55c6e1a09fc7a53f365207b0cc3e22393
Boris Zupancic <105008019+BorisZupancic@users.noreply.github.com>
2026-02-01 20:19:11 -0500

    Fixing syncthing

--- modules/services/syncthing.nix
+++ modules/services/syncthing.nix
@@ -1,18 +1,21 @@
 { pkgs, ... }:
 
 {
-  # 1. System-level: Open the ports
-  networking.firewall.allowedTCPPorts = [ 22000 ];
-  networking.firewall.allowedUDPPorts = [ 22000 21027 ];
+  services.syncthing = {
+    enable = true;
+    user = "borisz";
+    group = "users";
+    # We remove configDir and dataDir to let Nix use its safe defaults
+    # By default, it will now use /home/borisz/.config/syncthing correctly
+    openDefaultPorts = true;
+  };
 
-  # 2. System-level: Install the tray app globally 
-  environment.systemPackages = [ pkgs.syncthingtray ];
+  # This ensures the service has a clear path and doesn't look at /var/lib
+  systemd.services.syncthing.environment.HOME = "/home/borisz";
 
-  # 3. User-level: Configure the actual sync service
+  environment.systemPackages = [ pkgs.syncthing pkgs.syncthingtray ];
+
   home-manager.users.borisz = {
-    services.syncthing = {
-      enable = true;
-      tray.enable = true;
-    };
+    services.syncthing.tray.enable = true;
   };
 }
