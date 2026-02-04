d94dc3f45efeb1183f6abb8bf4f3c0bbcea25001
Boris Zupancic <105008019+BorisZupancic@users.noreply.github.com>
2026-02-04 16:07:02 -0500

    Removing setup for Arch Linux partition

--- modules/services/syncthing.nix
+++ modules/services/syncthing.nix
@@ -5,17 +5,35 @@
     enable = true;
     user = "borisz";
     group = "users";
-    # We remove configDir and dataDir to let Nix use its safe defaults
-    # By default, it will now use /home/borisz/.config/syncthing correctly
+    configDir = "/home/borisz/.config/syncthing";
+    dataDir = "/home/borisz/Sync";
     openDefaultPorts = true;
   };
 
-  # This ensures the service has a clear path and doesn't look at /var/lib
-  systemd.services.syncthing.environment.HOME = "/home/borisz";
+  systemd.services.syncthing = {
+    # 1. Ensure it starts on boot
+    wantedBy = [ "multi-user.target" ]; 
+    
+    # 2. Wait for the network to be ready
+    after = [ "network.target" "network-online.target" ];
+    wants = [ "network-online.target" ];
+
+    # 3. The environment
+    environment.HOME = "/home/borisz";
+
+    # 4. Restart if it fails during the boot scramble
+    serviceConfig = {
+      Restart = "on-failure";
+      RestartSec = "10s";
+    };
+  };
 
   environment.systemPackages = [ pkgs.syncthing pkgs.syncthingtray ];
 
   home-manager.users.borisz = {
-    services.syncthing.tray.enable = true;
+    services.syncthing.tray = {
+      enable = true;
+      command = "syncthingtray --wait"; #waits for syncthing to start
+    };
   };
 }
--- modules/system/boot.nix
+++ modules/system/boot.nix
@@ -8,21 +8,12 @@
     device = "nodev";
     efiSupport = true;
     useOSProber = false; # The "Speed Boost"
-    
-    # Manual entry for Arch Linux
-    extraEntries = ''
-      menuentry "Arch Linux" {
-        insmod gzio
-        insmod part_gpt
-        insmod ext2
-        insmod nvme
-        search --no-floppy --fs-uuid --set=root da6ba101-6524-450e-b95e-abfe1a19dfd4
-        linux /boot/vmlinuz-linux root=UUID=da6ba101-6524-450e-b95e-abfe1a19dfd4 rw
-        initrd /boot/initramfs-linux.img
-      }
+   
+    extraConfig = ''
+      insmod nvme
     '';
-  };
 
+  };
 
   # Use latest kernel.
   boot.kernelPackages = pkgs.linuxPackages_latest;
